1dccc569f3aadc88d49b5dca03ca48155ed00f18
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2018-12-15 10:24:40 -0600

    Installed Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {Customer} from './app.model'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.view.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'customerTickets';
+  CustomerModel : Customer = new Customer();
 }
---
+++ src/app/app.model.ts
@@ -0,0 +1,8 @@
+export class Customer {
+    CustomerFirstName : string = "";
+    CustomerLastName : string = "";
+    CustomerEmail : string = "";
+    TicketSubject : string = "";
+    TicketPriority : string = "";
+    TicketDescription : string = "";
+  }
